724b2d5dd92899f0d845d3751bd96974e97509bc
Unknown <gyomberattila@gmail.com>
2019-01-26 20:04:21 +0100

    pages generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TranslateComponent } from './translate/translate.component';
+import { DictionaryComponent } from './dictionary/dictionary.component';
+import { AddictionComponent } from './addiction/addiction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TranslateComponent,
+    DictionaryComponent,
+    AddictionComponent
   ],
   imports: [
     BrowserModule
