204e03cd5a90fb0f95c2f91dbc877774b6330861
Unknown <gyomberattila@gmail.com>
2019-01-26 21:28:47 +0100

    dictionary finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dictionary';
+
+  ngOnInit() {
+    document.body.classList.add('bg-img');
+  }
 }
--- src/app/dictionary/dictionary.component.ts
+++ src/app/dictionary/dictionary.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatSort, MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-dictionary',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DictionaryComponent implements OnInit {
 
+  displayedColumns: string[] = ['select', 'id', 'english', 'hungarian', 'partOfSpeech', 'synonym', 'example'];
+  dataSource = new MatTableDataSource(dictionary);
+  selection = new SelectionModel<DictionaryElement>(true, []);
+
   constructor() { }
 
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  @ViewChild(MatSort) sort: MatSort;
+
   ngOnInit() {
+    this.dataSource.sort = this.sort;
   }
 
 }
+
+export interface DictionaryElement {
+  id: number,
+  english: string;
+  hungarian: string;
+  partOfSpeech: string;
+  synonym: string;
+  example: string;
+}
+
+const dictionary: DictionaryElement[] = [
+  { id: 1, english: 'zxvzxv', hungarian: 'vzxv', partOfSpeech: 'dvxcvxc', synonym: 'asdasd', example: 'djggjsdkl' },
+  { id: 2, english: 'zxvzxv', hungarian: 'vzxv', partOfSpeech: 'dvxcvxc', synonym: 'asdasd', example: 'djggjsdkl' },
+  { id: 3, english: 'zxvzxv', hungarian: 'vzxv', partOfSpeech: 'dvxcvxc', synonym: 'asdasd', example: 'djggjsdkl' },
+  { id: 4, english: 'zxvzxv', hungarian: 'vzxv', partOfSpeech: 'dvxcvxc', synonym: 'asdasd', example: 'djggjsdkl' }
+];
